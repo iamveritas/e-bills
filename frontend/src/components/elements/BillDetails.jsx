aec3512fe41dbbaf7b2f1571fce7f6626accdf2e
mehfoozsalik <mehfoozsalik@gmail.com>
2023-09-11 00:01:58 +0500

    new changes on billing

--- frontend/src/components/elements/BillDetails.jsx
+++ frontend/src/components/elements/BillDetails.jsx
@@ -1,30 +1,56 @@
-import React from "react";
+import React, { useContext } from "react";
 import IconHolder from "./IconHolder";
+import { MainContext } from "../../context/MainContext";
 
-function BillDetails({data, icon}) {
-    return (
-        <>
-            {data.map((items, i) => {
-                let sign = items.to_payee ? "+" : "-";
-                return (
-                    <div key={i} className="home-container-bills-container">
-                        <IconHolder icon={icon}/>
-                        <div className="details">
-                            <span className="name">{items.name}</span>
-                            <span className="date">{items.date_of_issue}</span>
-                        </div>
-                        <div className="currency-details">
-                            <div className={sign === "+" ? "amount" : "amount red"}>
-                                <span>{items.sign}</span>
-                                <span>{items.amount_numbers}</span>
-                            </div>
-                            <span className="currency">{items.currency_code}</span>
-                        </div>
-                    </div>
-                );
-            })}
-        </>
-    );
+function BillDetails({ data, icon }) {
+  const { peer_id } = useContext(MainContext);
+  return (
+    <>
+      {data.map((items, i) => {
+        let sign;
+        // let name = "";
+        if (peer_id == items.drawee.peer_id) {
+          //   name = `${items?.drawee?.name} has to pay ${items?.payee?.name}`;
+          sign = "-";
+        } else if (peer_id == items.drawer.peer_id) {
+          //   name = `${items.drawee.name} ${items.payee.name}`;
+          sign = "x";
+        } else if (peer_id == items.payee.peer_id) {
+          //   name = `${items.drawee.name} ${items.payee.name}`;
+          sign = "+";
+        } else if (peer_id == items.endorsee.peer_id) {
+          //   name = `${items.drawee.name} ${items.payee.name}`;
+          sign = "+";
+        }
+        return (
+          <div key={i} className="home-container-bills-container">
+            <IconHolder icon={icon} />
+            <div className="details">
+              <span className="name">{items.name}</span>
+              <span className="date">{items.date_of_issue}</span>
+            </div>
+            <div className="currency-details">
+              <div
+                className={
+                  sign === "+"
+                    ? "amount"
+                    : sign === "x"
+                    ? "amount grey"
+                    : sign === "-"
+                    ? "amount red"
+                    : ""
+                }
+              >
+                <span>{sign === "x" ? "" : sign}</span>
+                <span>{items.amount_numbers}</span>
+              </div>
+              <span className="currency">{items.currency_code}</span>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default BillDetails;
--- frontend/src/context/MainContext.jsx
+++ frontend/src/context/MainContext.jsx
@@ -1,17 +1,31 @@
-import React, {createContext, useState} from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 const MainContext = createContext();
 
-function MainProvider({children}) {
-    const [current, setCurrent] = useState("home");
-    const handlePage = (page) => {
-        setCurrent(page);
-    };
-    return (
-        <MainContext.Provider value={{current, handlePage}}>
-            {children}
-        </MainContext.Provider>
-    );
+function MainProvider({ children }) {
+  const [current, setCurrent] = useState("home");
+  const handlePage = (page) => {
+    setCurrent(page);
+  };
+  const [peer_id, setPeerId] = useState({
+    id: String,
+  });
+  // Set peer id
+  useEffect(() => {
+    fetch("http://localhost:8000/identity/peer_id/return")
+      .then((res) => res.json())
+      .then((data) => {
+        setPeerId(data.id);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  return (
+    <MainContext.Provider value={{ peer_id, current, handlePage }}>
+      {children}
+    </MainContext.Provider>
+  );
 }
 
-export {MainContext, MainProvider};
+export { MainContext, MainProvider };
