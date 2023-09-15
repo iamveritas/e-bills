ae42171fadd21ce2c8697619c50fb22282e689f0
Mykyta Tymko <tymkomykyta2108@gmail.com>
2023-09-16 00:23:21 +0200

    E-Bill
    Style.
    
    Signed-off-by: Mykyta Tymko <tymkomykyta2108@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 
 import attachment from "./assests/attachment.svg";
 // components
@@ -11,7 +11,7 @@ import IssuePage from "./components/pages/IssuePage";
 import AcceptPage from "./components/pages/AcceptPage";
 import RepayPage from "./components/pages/RepayPage";
 import Bill from "./components/pages/Bill";
-import { MainContext, MainProvider } from "./context/MainContext";
+import {MainContext, MainProvider} from "./context/MainContext";
 import HomePage from "./components/pages/HomePage";
 import ActivityPage from "./components/pages/ActivityPage";
 import EndorsePage from "./components/pages/EndorsePage";
@@ -19,127 +19,127 @@ import ReqPaymentPage from "./components/pages/ReqPaymentPage";
 import ReqAcceptPage from "./components/pages/ReqAcceptPage";
 
 export default function App() {
-  const { current } = useContext(MainContext);
-  // Set data for bill issue
-  const [data, setData] = useState({
-    maturity_date: "",
-    payee_name: "",
-    currency_code: "sats",
-    amount_numbers: "",
-    drawee_name: "",
-    drawer_name: "",
-    place_of_drawing: "",
-    place_of_payment: "",
-    bill_jurisdiction: "",
-    date_of_issue: "",
-    language: "en",
-    drawer_is_payee: false,
-    drawer_is_drawee: false,
-  });
+    const {current} = useContext(MainContext);
+    // Set data for bill issue
+    const [data, setData] = useState({
+        maturity_date: "",
+        payee_name: "",
+        currency_code: "sats",
+        amount_numbers: "",
+        drawee_name: "",
+        drawer_name: "",
+        place_of_drawing: "",
+        place_of_payment: "",
+        bill_jurisdiction: "",
+        date_of_issue: "",
+        language: "en",
+        drawer_is_payee: false,
+        drawer_is_drawee: false,
+    });
 
-  const [identity, setIdentity] = useState({
-    name: String,
-    date_of_birth: String,
-    city_of_birth: String,
-    country_of_birth: String,
-    email: String,
-    postal_address: String,
-    public_key_pem: String,
-    private_key_pem: String,
-    bitcoin_public_key: String,
-    bitcoin_private_key: String,
-  });
-  // Set identity
-  useEffect(() => {
-    fetch("http://localhost:8000/identity/return")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setIdentity(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
+    const [identity, setIdentity] = useState({
+        name: String,
+        date_of_birth: String,
+        city_of_birth: String,
+        country_of_birth: String,
+        email: String,
+        postal_address: String,
+        public_key_pem: String,
+        private_key_pem: String,
+        bitcoin_public_key: String,
+        bitcoin_private_key: String,
+    });
+    // Set identity
+    useEffect(() => {
+        fetch("http://localhost:8000/identity/return")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setIdentity(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
 
-  const [contacts, setContacts] = useState([]);
-  // Set contacts
-  useEffect(() => {
-    fetch("http://localhost:8000/contacts/return")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setContacts(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
+    const [contacts, setContacts] = useState([]);
+    // Set contacts
+    useEffect(() => {
+        fetch("http://localhost:8000/contacts/return")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setContacts(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
 
-  const [bills_list, setBillsList] = useState([]);
-  // Set bills
-  useEffect(() => {
-    fetch("http://localhost:8000/bills/return")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setBillsList(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
+    const [bills_list, setBillsList] = useState([]);
+    // Set bills
+    useEffect(() => {
+        fetch("http://localhost:8000/bills/return")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setBillsList(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
 
-  const changeHandle = (e) => {
-    let value = e.target.value;
-    let name = e.target.name;
-    setData({ ...data, [name]: value });
-  };
-  const handleChangeDrawerIsPayee = (e) => {
-    let value = !data.drawer_is_payee;
-    let name = e.target.name;
-    setData({ ...data, [name]: value });
-  };
-  const handleChangeDrawerIsDrawee = (e) => {
-    let value = !data.drawer_is_drawee;
-    let name = e.target.name;
-    setData({ ...data, [name]: value });
-  };
-  const activePage = () => {
-    switch (current) {
-      case "home":
-        return <HomePage bills_list={bills_list} />;
-      case "activity":
-        return <ActivityPage bills_list={bills_list} />;
-      case "reqaccept":
-        return <ReqAcceptPage />;
-      case "reqpayment":
-        return <ReqPaymentPage />;
-      case "endorse":
-        return <EndorsePage />;
-      case "issue":
-        return (
-          <IssuePage
-            handleChangeDrawerIsDrawee={handleChangeDrawerIsDrawee}
-            contacts={contacts}
-            identity={identity}
-            data={data}
-            changeHandle={changeHandle}
-            handleChangeDrawerIsPayee={handleChangeDrawerIsPayee}
-          />
-        );
-      case "accept":
-        return <AcceptPage identity={identity} data={data} />;
-      case "repay":
-        return (
-          <RepayPage contacts={contacts} identity={identity} data={data} />
-        );
-      case "bill":
-        return <Bill identity={identity} data={data} />;
-      case "dont":
-        return <HomePage bills_list={bills_list} />;
-    }
-  };
+    const changeHandle = (e) => {
+        let value = e.target.value;
+        let name = e.target.name;
+        setData({...data, [name]: value});
+    };
+    const handleChangeDrawerIsPayee = (e) => {
+        let value = !data.drawer_is_payee;
+        let name = e.target.name;
+        setData({...data, [name]: value});
+    };
+    const handleChangeDrawerIsDrawee = (e) => {
+        let value = !data.drawer_is_drawee;
+        let name = e.target.name;
+        setData({...data, [name]: value});
+    };
+    const activePage = () => {
+        switch (current) {
+            case "home":
+                return <HomePage bills_list={bills_list}/>;
+            case "activity":
+                return <ActivityPage bills_list={bills_list}/>;
+            case "reqaccept":
+                return <ReqAcceptPage/>;
+            case "reqpayment":
+                return <ReqPaymentPage/>;
+            case "endorse":
+                return <EndorsePage/>;
+            case "issue":
+                return (
+                    <IssuePage
+                        handleChangeDrawerIsDrawee={handleChangeDrawerIsDrawee}
+                        contacts={contacts}
+                        identity={identity}
+                        data={data}
+                        changeHandle={changeHandle}
+                        handleChangeDrawerIsPayee={handleChangeDrawerIsPayee}
+                    />
+                );
+            case "accept":
+                return <AcceptPage identity={identity} data={data}/>;
+            case "repay":
+                return (
+                    <RepayPage contacts={contacts} identity={identity} data={data}/>
+                );
+            case "bill":
+                return <Bill identity={identity} data={data}/>;
+            case "dont":
+                return <HomePage bills_list={bills_list}/>;
+        }
+    };
 
-  return <>{activePage()}</>;
+    return <>{activePage()}</>;
 }
--- frontend/src/components/pages/HomePage.jsx
+++ frontend/src/components/pages/HomePage.jsx
@@ -7,40 +7,40 @@ import BillDetails from "../elements/BillDetails";
 import Payments from "../elements/Payments";
 import bills from "../../assests/bills.svg";
 
-export default function HomePage({ bills_list }) {
-  return (
-    <div className="home">
-      <HomeHeader />
-      <div className="home-container">
-        <div className="home-container-amount">
-          <Amount color="38A665" currency="BTC" amount="1.021" degree="0" />
-          <hr className="home-container-amount-line" />
-          <Amount color="C30000" currency="BTC" amount="1.011" degree="180" />
+export default function HomePage({bills_list}) {
+    return (
+        <div className="home">
+            <HomeHeader/>
+            <div className="home-container">
+                <div className="home-container-amount">
+                    <Amount color="38A665" currency="BTC" amount="1.021" degree="0"/>
+                    <hr className="home-container-amount-line"/>
+                    <Amount color="C30000" currency="BTC" amount="1.011" degree="180"/>
+                </div>
+                <div className="home-container-heading">
+                    <SubHeading rotate="0" route="issue" color="38A665" currency="Bill"/>
+                    <SubHeading rotate="180" route="dont" color="C30000" currency="IOU"/>
+                </div>
+                <div className="home-container-total">
+                    <SubTotal color="a3a3a3" currency="BTC" amount="7.01"/>
+                </div>
+                <div className="line"></div>
+                <div className="home-container-bills">
+                    <BillDetails
+                        color="a3a3a3"
+                        data={bills_list?.slice(0, 4)}
+                        icon={bills}
+                    />
+                </div>
+                {bills_list?.length > 4 && (
+                    <div className="home-container-payments">
+                        <Payments
+                            payments="4 Recent Payments"
+                            history="Full Payment History"
+                        />
+                    </div>
+                )}
+            </div>
         </div>
-        <div className="home-container-heading">
-          <SubHeading rotate="0" route="issue" color="38A665" currency="Bill" />
-          <SubHeading rotate="180" route="dont" color="C30000" currency="IOU" />
-        </div>
-        <div className="home-container-total">
-          <SubTotal color="a3a3a3" currency="BTC" amount="7.01" />
-        </div>
-        <div className="line"></div>
-        <div className="home-container-bills">
-          <BillDetails
-            color="a3a3a3"
-            data={bills_list?.slice(0, 4)}
-            icon={bills}
-          />
-        </div>
-        {bills_list?.length > 4 && (
-          <div className="home-container-payments">
-            <Payments
-              payments="4 Recent Payments"
-              history="Full Payment History"
-            />
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    );
 }
