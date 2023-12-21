e2120bddeb4cc1c538f7ec4d9515c240dd510bd7
upoi <lukasdrescher@gmx.de>
2023-12-21 19:20:09 +0100

    remove peer_id display on contacts page

--- frontend/src/components/pages/Contact.jsx
+++ frontend/src/components/pages/Contact.jsx
@@ -42,7 +42,6 @@ export default function Contact() {
                     return (
                         <div key={i} className="contact-body-user">
                             <span>{d.name}</span>
-                            <span>{d.peer_id}</span>
                             <img onClick={() => showPopUp(true, <EditContact old_name={d.name}/>)} src={editBtn}/>
                             <img onClick={() => handleDelete(d.name)} src={deleteBtn}/>
                         </div>
